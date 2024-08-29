c72589bb67352d4a5346abe664b4d5d53051a0d8
RishabhHM <rishabh.hm06@gmail.com>
2024-08-29 03:27:25 -0400

    ➕ chore : Requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "hate-speech-classification",
+    version = "0.0.1",
+    author = "Rishabh Madani",
+    author_email = "rishabh.hm06@gmail.com",
+    packages = find_packages(),
+    install_requires = [] 
+)
